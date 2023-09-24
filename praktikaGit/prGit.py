4d325a34694b5219d4b989609c705ea70b3ae895
BareonZahar <zatst2000@yandex.ru>
2023-09-24 18:36:04 +0300

    vibor3

--- praktikaGit/prGit.py
+++ praktikaGit/prGit.py
@@ -6,6 +6,10 @@ text1 = 'new text'
 def nyt(a, b):
     return a - b
 
+def nut(a, b):
+    return a - b
+
 if __name__ == '__main__':
     print(gipog(4, 5))
     print(nyt(13, 9))
+    print(nut(6, 5))
